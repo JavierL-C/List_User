6f0da3ddde24055e0498f2c5e59149720e4ee46e
JLumbi <javier.lumbi@siprocal.com>
2024-10-14 10:35:23 -0600

    Initial Commit

---
+++ ListUserApp/ListUserAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListUserAppApp.swift
+//  ListUserApp
+//
+//  Created by Javier Lumbi on 14/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ListUserAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
